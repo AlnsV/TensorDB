e973205bde69f4af58b6165fa87b839ae61f10aa
joseph <josephgonowak97@gmail.com>
2021-04-09 17:13:05 -0400

    Modifying the init and the setup

--- __init__.py
+++ __init__.py
@@ -1,1 +1,4 @@
-
+from bitacore.file_db import FileDB
+from store_core.data_handler.files_store import FilesStore
+from store_core.s3_handler.s3_handler import S3Handler
+from store_core.zarr_handler.zarr_store import ZarrStore
